9fadecb1622fa07c559a173fd77ba9d331fa096c
sakshikakde <kakdesakshi@gmail.com>
2022-04-18 01:02:34 -0400

    environment manager and tests

---
+++ env_manager.py
@@ -0,0 +1,82 @@
+from common_utils import *
+
+class HighwayEnvManager():
+    def __init__(self, device):
+        self.device = device
+        self.env = gym.make('highway-v0').unwrapped
+        self.configure()
+        self.env.reset()
+        self.current_screen = None
+        self.observation = self.env.reset()
+        self.done = False
+
+    def configure(self):
+        config = {
+        "observation": {
+            "type": "GrayscaleObservation",
+            "observation_shape": (128, 64),
+            "weights": [0.2989, 0.5870, 0.1140],
+            "stack_size": 4,
+        },
+        "policy_frequency": 2
+        }
+        self.env.configure(config)
+
+    def reset(self):
+        self.env.reset()
+        self.observation = self.env.reset()
+        self.current_screen = None
+
+    def close(self):
+        self.env.close()
+
+    def render(self, mode='human'):
+        return self.env.render(mode)
+
+    def num_actions_available(self):
+        return self.env.action_space.n
+
+    def take_action(self, action):        
+        self.observation, reward, self.done, _ = self.env.step(action.item())
+        return torch.tensor([reward], device=self.device)
+
+    def just_starting(self):
+        return self.current_screen is None
+
+    def get_state(self):
+        self.current_screen = self.get_processed_screen()
+        return self.current_screen
+
+    def get_screen_height(self):
+        screen = self.get_processed_screen()
+        return screen.shape[2]
+
+    def get_screen_width(self):
+        screen = self.get_processed_screen()
+        return screen.shape[3]
+
+    def get_processed_screen(self):
+        screen = self.observation # PyTorch expects CHW
+        # screen = self.crop_screen(screen)
+        return self.transform_screen_data(screen)
+
+    def crop_screen(self, screen):
+        screen_height = screen.shape[1]
+        # Strip off top and bottom
+        top = int(screen_height * 0.4)
+        bottom = int(screen_height * 0.8)
+        screen = screen[:, top:bottom, :]
+        return screen
+
+    def transform_screen_data(self, screen):       
+        # Convert to float, rescale, convert to tensor
+        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+        screen = torch.from_numpy(screen)
+
+        # Use torchvision package to compose image transforms
+        transform = T.Compose([
+            T.ToPILImage(),
+            T.ToTensor()
+        ])
+
+        return transform(screen).unsqueeze(0).to(self.device) # add a batch dimension (BCHW)
---
+++ tests/test_env_manager.py
@@ -0,0 +1,33 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path( __file__ ).parent.joinpath('..')))
+from env_manager import *
+
+
+def test_init_data(em, device):
+    screen = em.get_processed_screen().cpu().numpy()
+    em.close()
+
+    _, axes = plt.subplots(ncols=4, figsize=(12, 5))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(screen[0, i, ...].T, cmap=plt.get_cmap('gray'))
+    plt.savefig('./tests/test_init.png')
+
+def test_step_data(em, device):
+    action = random.randrange(em.num_actions_available())
+    em.take_action(torch.tensor([action]).to(device))
+    screen = em.get_processed_screen().cpu().numpy()
+    em.close()
+    
+    _, axes = plt.subplots(ncols=4, figsize=(12, 5))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(screen[0, i, ...].T, cmap=plt.get_cmap('gray'))
+    plt.savefig('./tests/test_step.png')
+
+
+if __name__ == "__main__":
+    print("testing environment manager...")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    em = HighwayEnvManager(device)
+    test_init_data(em, device)
+    test_step_data(em, device)
