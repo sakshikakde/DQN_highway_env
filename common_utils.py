0e212aa0ff689fabe67d472f8132e0b0c04664e3
sakshikakde <kakdesakshi@gmail.com>
2022-04-18 12:28:50 -0400

    common utils

--- common_utils.py
+++ common_utils.py
@@ -14,5 +14,52 @@ import torch.nn.functional as F
 import torchvision.transforms as T   
 import highway_env
 
-# is_ipython = 'inline' in matplotlib.get_backend()
-# if is_ipython: from IPython import display
+from models.dqn_conv import DQN as DQN
+from experience import *
+from replay_mem import *
+from strategy import *
+from agent import *
+from env_manager import *
+from q_value import *
+
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython: from IPython import display
+
+def plot(values, moving_avg_period):
+    plt.figure(2)
+    plt.clf()        
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(values)
+
+    moving_avg = get_moving_average(moving_avg_period, values)
+    plt.plot(moving_avg)    
+    plt.pause(0.001)
+    print("Episode", len(values), "\n", \
+        moving_avg_period, "episode moving avg:", moving_avg[-1])
+    if is_ipython: display.clear_output(wait=True)
+
+
+def get_moving_average(period, values):
+    values = torch.tensor(values, dtype=torch.float)
+    if len(values) >= period:
+        moving_avg = values.unfold(dimension=0, size=period, step=1) \
+            .mean(dim=1).flatten(start_dim=0)
+        moving_avg = torch.cat((torch.zeros(period-1), moving_avg))
+        return moving_avg.numpy()
+    else:
+        moving_avg = torch.zeros(len(values))
+        return moving_avg.numpy()
+
+def extract_tensors(experiences):
+    # Convert batch of Experiences to Experience of batches
+    batch = Experience(*zip(*experiences))
+
+    t1 = torch.cat(batch.state)
+    t2 = torch.cat(batch.action)
+    t3 = torch.cat(batch.reward)
+    t4 = torch.cat(batch.next_state)
+
+    return (t1,t2,t3,t4)
