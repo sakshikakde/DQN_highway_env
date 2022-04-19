d4e914770a9017f86874c20ced30557229571a5f
sakshikakde <kakdesakshi@gmail.com>
2022-04-19 00:04:55 -0400

    datatype bugfix

--- train.py
+++ train.py
@@ -16,9 +16,10 @@ def train_epoch(opt, em, agent, policy_net, target_net, memory, device, optimize
 
             current_q_values = QValues.get_current(policy_net, states, actions)
             next_q_values = QValues.get_next(target_net, next_states)
-            target_q_values = (next_q_values * opt.gamma) + rewards
+            target_q_values = ((next_q_values * opt.gamma) + rewards).type(torch.float32)
 
             loss = criterion(current_q_values, target_q_values.unsqueeze(1))
+            print(loss)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
