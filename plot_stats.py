b04e9c7c77c1e79ac3c26800c7b769e054ab8333
sakshikakde <kakdesakshi@gmail.com>
2022-04-22 11:07:49 -0400

    plot utils updated

--- plot_stats.py
+++ plot_stats.py
@@ -1,18 +1,24 @@
 from common_utils import *
 
 def plot_stats(csv_file_name, opt):
+
     data = pd.read_csv(csv_file_name)
+    if not (data.columns[0] == 'Duration'):  
+        print("Adding headers")  
+        header_list = ["Duration", "Reward", "Loss"]
+        data = pd.read_csv(csv_file_name, names=header_list)
+        
     duration = data['Duration']
     reward = data['Reward']
     loss = data['Loss']
 
-    plot(duration, 3, opt, title = 'Training', y_label = 'Duration', name = 'plots/duration.png')
-    plot(reward, 3, opt, title = 'Training', y_label = 'Rewards',  name = 'plots/reward.png')
-    plot(loss, 3, opt, title = 'Training', y_label = 'Loss',  name = 'plots/loss.png')
+    plot(duration, 50, opt, title = 'Training', y_label = 'Duration', name = 'plots/duration.png')
+    plot(reward, 50, opt, title = 'Training', y_label = 'Rewards',  name = 'plots/reward.png')
+    plot(loss, 50, opt, title = 'Training', y_label = 'Loss',  name = 'plots/loss.png')
 
 def main():
     opt = parse_opts()
-    csv_file_name  = os.path.join(opt.save_folder, 'Apr-21-2022_1457_stats.csv')
+    csv_file_name  = os.path.join(opt.save_folder, opt.env, 'Apr-22-2022_0422_stats.csv')
     plot_stats(csv_file_name, opt)
 
 if __name__ == "__main__":
